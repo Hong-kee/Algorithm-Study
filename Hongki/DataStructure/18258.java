8813c546c7e05dc81733ac22da8d70f049786836
Hongki Kim <69206748+Hong-kee@users.noreply.github.com>
2022-02-02 14:04:51 +0900

    큐 2
    큐 2

--- Hongki/DataStructure/18258.java
+++ Hongki/DataStructure/18258.java
@@ -1,18 +1,13 @@
-/**
-* 추후 추가 예정
-*/
-package baekjoon.datastructure;
-
 import java.io.*;
 import java.util.*;
 
-public class Main18258 {
+public class Main {
 
     static int[] q = new int[2000000];
-
+    static StringBuilder sb = new StringBuilder();
     static int size = 0;
     static int front = 0;
-    static int rear = 0;
+    static int back = 0;
 
     public static void main(String[] args) throws IOException{
 
@@ -20,15 +15,52 @@ public class Main18258 {
         StringTokenizer st;
 
         int commandCnt = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < commandCnt; i++) {
-            if (st.nextToken() == "push") {}
-            else if (st.nextToken() == "pop") {}
-            else if (st.nextToken() == "size") {}
-            else if (st.nextToken() == "empty") {}
-            else if (st.nextToken() == "front") {}
-            else if (st.nextToken() == "back") {}
+        while(commandCnt --> 0) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+
+            if (str.equals("push")) push(Integer.parseInt(st.nextToken()));
+            else if (str.equals("pop")) pop();
+            else if (str.equals("size")) size();
+            else if (str.equals("empty")) empty();
+            else if (str.equals("front")) front();
+            else if (str.equals("back")) back();
         }
+        System.out.println(sb);
+    }
+
+    static void push(int num) {
+        q[back] = num;
+        ++back;
+        ++size;
+    }
+
+    static void pop() {
+        if (size == 0) sb.append(-1).append('\n');
+        else {
+            sb.append(q[front]).append('\n');
+            front++;
+            size--;
+        }
+    }
+
+    static void size() {
+        sb.append(size).append('\n');
+    }
+
+    static void empty() {
+        if (size == 0) sb.append(1).append('\n');
+        else sb.append(0).append('\n');
+    }
+
+    static void front() {
+        if(size == 0) sb.append(-1).append('\n');
+        else sb.append(q[front]).append('\n');
+    }
+
+    static void back() {
+        if (size == 0) sb.append(-1).append('\n');
+        else sb.append(q[back - 1]).append('\n');
     }
 }

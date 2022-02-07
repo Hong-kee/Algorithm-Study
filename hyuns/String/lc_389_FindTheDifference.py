7bb550061da7ef1f79582c62f7a85367e09ce3c0
hyunsoo <hyuns9411@gmail.com>
2022-02-07 15:54:09 +0900

    Upload lc_389

---
+++ hyuns/String/lc_389_FindTheDifference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s = sorted(s)
+        t = sorted(t)
+        
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return t[i]
+        
+        return t[-1]

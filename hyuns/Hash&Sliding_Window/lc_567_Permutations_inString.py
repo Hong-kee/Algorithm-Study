9e7475362f66567ad2847ee83e9d9f67b6dce22c
hyunsoo <hyuns9411@gmail.com>
2022-02-11 17:18:03 +0900

    Add Sol lc_567

--- hyuns/Hash&Sliding_Window/lc_567_Permutations_inString.py
+++ hyuns/Hash&Sliding_Window/lc_567_Permutations_inString.py
@@ -1,25 +1,75 @@
-from itertools import permutations
+# Sol - sorting
+# Runtime ; 5747ms, Memory 14M
+
+def checkInclusion(self, s1: str, s2: str) -> bool:
+    s1 = sorted(s1)
+    for i in range(len(s2)-len(s1)+1):
+        if sorted(s2[i:i+len(s1)]) == s1:
+            return True      
+    return False       
 
 
-from itertools import permutations
+# Sol - by ASCII of char, Rolling hash
+# Runtime ; 227ms Memory 14.2M
+
+def checkInclusion(s1, s2):
+
+    A = [ord(x) - ord("a") for x in s1]
+    B = [ord(x) - ord("a") for x in s2]
+
+    target = [0] * 26
+    for i in s1:
+        target[i] += 1
+    
+    window = [0] * 26
+    
+    for i, s in enumerate(B):
+
+        # Sliding Window
+
+        # add
+        window[s] += 1
+
+        # remove
+        if i >= len(A):
+            target[B[i-len(A)]] -= 1
+
+        # check sliding
+        if target == window:
+            return True
+
+    return False
+
+s1, s2 = input(), input()
+print(checkInclusion(s1, s2))
+
+
+
+
+# Sol - Counter
 from collections import Counter
 def checkInclusion(s1, s2):
     if len(s1) > len(s2): return False
 
     target, window = Counter(s1), Counter()
-    start = 0
     
     for idx, s in enumerate(s2):
 
+        # Sliding Window
+
+        # add
         window[s] += 1
 
+        # remove
         if idx >= len(s1):
-            window[s2[start]] -= 1
-            if window[s2[start]] == 0: del window[s2[start]]
-            start += 1
+            window[s1[idx-len(s1)]] -= 1
+            if not window[s1[idx-len(s1)]]:
+                del window[s1[idx-len(s1)]]
 
+        # check after sliding
         if window == target:
             return True
+
     return False
 
 s1 = input()

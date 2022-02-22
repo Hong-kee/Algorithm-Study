eafebbf21542e3b7ccde35ff4e0a6c7545c14389
hyunsoo <hyuns9411@gmail.com>
2022-02-22 18:00:38 +0900

    Upload 15683_감시

--- hyuns/Implement/15683_감시.py
+++ hyuns/Implement/15683_감시.py
@@ -1,37 +1,58 @@
-# 다시
-# def get_camera_location():
-#     camera = []
-#     for i in range(len(graph)):
-#         for j in range(len(graph[i])):
-#             if 1 <= graph[i][j] <= 5:
-#                 camera.append([i,j])
-#     return camera
+from copy import deepcopy
 
-# def monitor_dir(x,y):
-#     if graph[x][y] == 1:
-#         return (0,1), (1,0), (0,-1), (-1,0)
+def fill_zeroToSee(dir, tmp_graph, x, y):
 
-# def camera_monitor(x,y):
+    for i in dir:
+        zeroToSee = 1
+        if i == 0:
+            while y+zeroToSee != M and tmp_graph[x][y+zeroToSee] != 6:
+                tmp_graph[x][y+zeroToSee] = '#'
+                zeroToSee += 1
+        elif i == 1:
+            while (x+zeroToSee) != N and tmp_graph[x+zeroToSee][y] != 6:
+                tmp_graph[x+zeroToSee][y] = '#'
+                zeroToSee += 1
+        elif i == 2:
+            while (y-zeroToSee) != -1 and tmp_graph[x][y-zeroToSee] != 6:
+                tmp_graph[x][y-zeroToSee] = '#'
+                zeroToSee += 1
+        else:
+            while (x-zeroToSee) != -1 and tmp_graph[x-zeroToSee][y] != 6:
+                tmp_graph[x-zeroToSee][y] = '#'
+                zeroToSee += 1
+    return tmp_graph
 
-#     maxs = 0
-#     for i, j in monitor_dir(x, y):
-#         sums = 0
-#         mx, my = x+i, y+i
-#         if 0<=mx<=n-1 and 0<=my<=m-1:
-#             if graph[mx][my] == 0:
-#                 graph[mx][my] = "#"
-#                 sums += 1
-#         maxs = max(sums, maxs)
-#     return maxs
+def dfs(answer, idx, graph, cctv_location, mode):
 
+    if idx == len(cctv_location):
+        return sum([arr.count(0) for arr in graph])
 
+    cctv_num, x, y = cctv_location[idx]
 
+    for dir in mode[cctv_num]:
+        tmp_graph = deepcopy(graph)
+        tmp_graph = fill_zeroToSee(dir, tmp_graph, x, y)
+        answer = min(answer, dfs(answer, idx+1, tmp_graph, cctv_location, mode))
+    
+    return answer
 
-# if __name__ == "__main__":
-#     n, m  = map(int, input().split())
-#     graph = [list(map(int, input().split())) for _ in range(n)]
+def main():
+    cctv_location = []
+    mode = [[], 
+    [[0], [1], [2], [3]],
+    [[0,2], [1,3]],
+    [[0,1], [1,2],[2,3],[3,0]],
+    [[0,1,2],[1,2,3],[2,3,0],[3, 0, 1]],
+    [[0,1,2,3]]
+    ]
+    for i in range(len(graph)):
+        for j in range(len(graph[i])):
+            if graph[i][j] in [1,2,3,4,5]:
+                cctv_location.append([graph[i][j], i,j])
 
-#     camera_location = get_camera_location()
+    return dfs(N*M, 0, graph, cctv_location, mode)
 
-#     for x, y in camera_location:
-#         answer += camera_monitor(camera_location)
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    graph = [list(map(int, input().split())) for _ in range(N)]
+    print(main())

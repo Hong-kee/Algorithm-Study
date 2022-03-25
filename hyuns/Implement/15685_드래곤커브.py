9cfd25d3a2c0776e50d0b08e2d2c4d8af344c17b
hyunsoo <hyuns9411@gmail.com>
2022-03-25 14:25:34 +0900

    Upload 15685_드래곤커브

--- hyuns/Implement/15685_드래곤커브.py
+++ hyuns/Implement/15685_드래곤커브.py
@@ -1,49 +1,5 @@
 
-
-def make_dragon(info):
-    x, y, d, g = info
-    d_mode = [(1,0), (0,1), (-1,0), (0,-1)]
-
-    if g == 0:
-        dx, dy = d_mode[d]
-        graph[x][y] = True
-        graph[x+dx][y+dy] = True
-        return
-
-    store_dir = [d]
-
-    for g_idx in range(g+1):
-        if g_idx == 0:
-            dx,dy = d_mode[store_dir[-1]]
-            graph[x][y] = True
-            x, y = x-dx, y-dy
-            store_dir.append(d)
-            d = d-1 if d-1>=0 else 3
-        
-        else:
-            dx, dy = d_mode[d]
-            graph[x][y] = True
-            x, y = x+dx, y+dy
-
-            nd = store_dir[-1]
-            new_store = [d]
-            for sd in store_dir[::-2]:
-                
-                move_d = sd-nd
-                dx, dy = d_mode[move_d] # 이전꺼에서 방향만 가져오기
-                x, y = x+dx, y+dy
-                graph[x][y] = True
-            
-            store_dir.extend(new_store)
-            d = move_d
-
-def main():
-
-    answer = 0
-    mc, mx = 102, -1
-    for info in dragon_info:
-        make_dragon(info)
-
+def count_square(answer, graph):
 
     for i in range(100):
         for j in range(100):
@@ -52,8 +8,43 @@ def main():
 
     return answer
 
+def make_direction(info):
+    x, y, d, gen = info
+    direct = [d]
+
+    for g in range(1, gen+1):
+        new_list = []
+        for direct_d in direct[::-1]:
+            direct_d = direct_d+1 if direct_d+1 <= 3 else 0
+            new_list.append(direct_d)
+        direct = direct + new_list
+    return direct
+
+def make_dragon(direction, info):
+    x, y, d, gen = info
+    graph[y][x] = True
+
+    for i in direction:
+        y, x = y+dy[i], x+dx[i]
+        if 0 <= y <= 100 and 0 <= x <= 100:
+            graph[y][x] = True
+
 if __name__ == "__main__":
     n = int(input())
     dragon_info = [list(map(int, input().split())) for _ in range(n)]
     graph = [[False] * 101 for _ in range(101)]
-    main()
+    dx = [1, 0, -1, 0]
+    dy = [0, -1, 0, 1]
+
+    answer = 0
+    dragon_direction = []
+
+    for info in dragon_info: # 방향 그려주기
+        dragon_direction.append(make_direction(info))
+    
+    for idx in range(len(dragon_direction)): # 점 찍기 (선 그리기)
+        make_dragon(dragon_direction[idx], dragon_info[idx])
+
+    answer = count_square(answer, graph) # 사각형 개수 세기
+    
+    print(answer)

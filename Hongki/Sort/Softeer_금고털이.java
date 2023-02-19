7e513d24d79f999f341856035698abd160f995b3
Hongki Kim <69206748+Hong-kee@users.noreply.github.com>
2023-02-19 20:44:51 +0900

    금고털이
    using Collections.sort and Comparable interface.

---
+++ Hongki/Sort/Softeer_금고털이.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] bagWeightAndJewelCounts = br.readLine().split(" ");
+        List<JewelInformation> jewelInformations = new ArrayList<>();
+
+        int bagWeight = Integer.parseInt(bagWeightAndJewelCounts[0]);
+        int answer = 0;
+
+        for (int i = 0; i < Integer.parseInt(bagWeightAndJewelCounts[1]); i++) {
+            JewelInformation jewelInformation = new JewelInformation();
+            String[] jewelInput = br.readLine().split(" ");
+
+            jewelInformation.setJewelPrice(Integer.parseInt(jewelInput[1]));
+            jewelInformation.setJewelSize(Integer.parseInt(jewelInput[0]));
+
+            jewelInformations.add(jewelInformation);
+        }
+
+        Collections.sort(jewelInformations);
+
+        for (int i = 0; i < jewelInformations.size(); i++) {
+            if (bagWeight > jewelInformations.get(i).getJewelSize()) {
+                bagWeight -= jewelInformations.get(i).getJewelSize();
+                answer += jewelInformations.get(i).getJewelSize() * jewelInformations.get(i).getJewelPrice();
+            } else {
+                if (bagWeight != 0) {
+                    answer += bagWeight * jewelInformations.get(i).getJewelPrice();
+                    bagWeight = 0;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static class JewelInformation implements Comparable<JewelInformation> {
+        int jewelPrice;
+        int jewelSize;
+
+        public int getJewelPrice() {
+            return jewelPrice;
+        }
+
+        public void setJewelPrice(int jewelPrice) {
+            this.jewelPrice = jewelPrice;
+        }
+
+        public int getJewelSize() {
+            return jewelSize;
+        }
+
+        public void setJewelSize(int jewelSize) {
+            this.jewelSize = jewelSize;
+        }
+
+        @Override
+        public int compareTo(JewelInformation o) {
+            return o.getJewelPrice() - getJewelPrice();
+        }
+
+    }
+
+}
